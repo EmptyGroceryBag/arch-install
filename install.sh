08e251476cf1528253981ab7377c0465ebeb1c48
EmptyGroceryBag <emptygrocerybag@hotmail.com>
2022-03-26 05:34:56 +0000

    works?

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
-timedatectl set-ntp true &&
-mkfs.ext4 /dev/sda1 &&
-mkswap /dev/sda2 &&
-swapon /dev/sda2 &&
+#!/bin/bash
+
 mount /dev/sda1 /mnt &&
-genfstab -U /mnt >> /mnt/etc/fstab &&
+cp ./locale.gen /etc/locale.gen &&
 ./packages.sh &&
-arch-chroot /mnt &&
+genfstab -U /mnt >> /mnt/etc/fstab &&
+
+arch-chroot /mnt /bin/bash <<"EOT"
 ln -sf /usr/share/zoneinfo/America/New_York &&
 hwclock --systohc &&
-cp ./locale.gen /etc/locale.gen &&
 locale-gen &&
 touch /etc/locale.conf &&
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
 echo "arch-test" >> /etc/hostname &&
 useradd -m joey &&
 cd /home/joey &&
-git clone https://github.com/EmptyGroceryBag/dotfiles &&
+git clone https://github.com/EmptyGroceryBag/dotfiles.git &&
+cd dotfiles &&
 cp -r .config ~/.config &&
 cp -r .vim ~/.vim &&
 cp .vimrc ~/.vimrc &&
 cp .zshrc ~/.zshrc &&
-grub-install --target=1386-pc /dev/sda &&
-exit
+grub-install --target=i386-pc /dev/sda
+EOT
